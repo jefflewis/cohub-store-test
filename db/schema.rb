57783b668a959824bda56d52e581f8c149d10d5e
Jeff Lewis <lewisj88@mac.com>
2015-05-17 23:23:16 -0700

    Update price column to be money type.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517212931) do
+ActiveRecord::Schema.define(version: 20150518062040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,10 +20,10 @@ ActiveRecord::Schema.define(version: 20150517212931) do
     t.string   "name"
     t.text     "description"
     t.integer  "quantity"
-    t.decimal  "price"
+    t.money    "price",       scale: 2
     t.text     "sku"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.text     "image"
   end
 
